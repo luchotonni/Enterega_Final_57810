afd3fd197a05d6380c2c8573c9a04bf82afc056f
Luciano Tonni <luchotonni@gmail.com>
2024-07-20 14:00:37 -0300

    nuevos cambios

--- Entrega-final-main/tienda/urls.py
+++ Entrega-final-main/tienda/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
     path('crear_producto/', views.crear_producto, name='crear_producto'),
     path('crear_categoria/', views.crear_categoria, name='crear_categoria'),
+    path('login/', include('django.contrib.auth.urls')),
 ]
